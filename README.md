9f997dce511ded6812c7b70d86f44bd825b9e47d
GillyRabutTsurwa <tsurwagilbert@gmail.com>
2020-08-10 18:40:36 -0500

    fetching jokes using the joke API. Loooking goo

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
 # Improving SEO
-- Don't actually know much about SEO. Il me faudra y beaucoup revenir.
-- Using the head method with that returns an object which contains:
-  - a title, which changes the title of the current page
-  - and meta, for meta stuff
-
+- Fetching our jokes from this awesome [API](https://icanhazdadjoke.com/api)
 For detailed explanation on how things work, check out [Nuxt.js docs](https://nuxtjs.org).
--- pages/Jokes/index.vue
+++ pages/Jokes/index.vue
@@ -3,7 +3,28 @@
 </template>
 
 <script>
+import axios from "@nuxtjs/axios"
 export default {
+  data() {
+    return {
+      jokes: []
+    }
+  },
+  // NEW:
+  async created() {
+    try {
+      // NOTEIMPORTANT: Faites attention à syntaxe peu differente pour utiliser axios avec Nuxt dessous.
+      const response = await this.$axios.$get("https://icanhazdadjoke.com/search", {
+        // NOTE: On peux aussi mettre cet objet headers au-dessous de declaration axios et déclarer l'objet ici
+        headers: {
+          Accept: "application/json"
+        }
+      });
+      console.log(response);
+    } catch (error) {
+      console.warn(error);
+    }
+  },
   head() {
     return {
       title: "Autre Page",
